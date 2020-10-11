858b416d513daf95bca92085d2d426ab06caf50d
Ann <oxid303@gmail.com>
2020-10-11 19:10:28 +0300

    Updates

---
+++ precache-manifest.8a9496b87f19ca802fa0f13dffe70c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3567fe7cb3aeb08b865909903d4261bf",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "34ca4c3ddd0e4b1efe1c",
+    "url": "/battleship/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4caff47e4be3f491336f",
+    "url": "/battleship/static/js/2.5dc208ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.5dc208ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ca4c3ddd0e4b1efe1c",
+    "url": "/battleship/static/js/main.977af3e3.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
